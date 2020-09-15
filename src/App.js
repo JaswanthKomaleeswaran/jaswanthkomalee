9313988b1bb2fbfec7cb73e32a7eceaf2bcd9fe8
JaswanthKomal10 <jaswanth1098@gmail.com>
2020-09-15 17:49:45 +0530

    Update Head

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import Header from './Components/Header';
-import Content from './Components/Content';
-import Footer from './Components/Footer';
-import Navigation from './Components/Navigation';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Header from "./Components/Header";
+import Content from "./Components/Content";
+import Footer from "./Components/Footer";
+import Navigation from "./Components/Navigation";
+import "./App.css";
 
 function App() {
   return (
